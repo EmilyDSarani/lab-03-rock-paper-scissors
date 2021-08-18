d51a4c7f1124b9f7d363bd03bedc2443737bc217
EmilyDSarani <emily.sarani@gmail.com>
2021-08-18 16:41:30 -0500

    my dev branch wasnt pushing

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // import functions and grab DOM elements
 
-// initialize global state
+const goButtonEl = document.getElementById ('pressgo');
+const resetButtonEl = document.getElementById ('reset');
+const userChoiceEl = document.getElementById('user-Choice');
+const computerChoiceEl = document.getElementById('computer-Choice');
+const winorloseEl = document.getElementById('win-lose');
+const winwinEl = document.getElementById ('wins');
+const loseloseEl = document.getElementById ('losses');
+
+let wins = 0;
+let losses = 0;
+let computerToss = null;
+
 
 // set event listeners 
   // get user input
