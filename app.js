f66269d1adde1888cf142c7798cb8f6a5f8248cf
EmilyDSarani <emily.sarani@gmail.com>
2021-08-18 18:50:26 -0500

    i forgot to add the divs back in

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 // import functions and grab DOM elements
-import { RPS } from './rps.js';
+import { rockpaperscissor } from './rps.js';
+import { didUserWin } from './rps.js';
 
 const goButtonEl = document.getElementById ('pressgo');
 const resetButtonEl = document.getElementById ('reset');
 const userChoiceEl = document.getElementById('user-Choice');
-const computerChoiceEl = document.getElementById('computer-Choice');
+const compChoiceEl = document.getElementById('computer-Choice');
 const winorloseEl = document.getElementById('win-lose');
 const winwinEl = document.getElementById ('wins');
 const loseloseEl = document.getElementById ('losses');
 
 let wins = 0;
 let losses = 0;
-let computerToss = null;
 
-goButtonEl.addEventListener('click',() => {
-    const threeThrow = RPS();
+goButtonEl.addEventListener('click', () => {
+  
+    const threeThrow = rockpaperscissor();
+    const selectorEl = document.querySelector('input:checked');
+    
+    const winnerwinner = didUserWin (selectorEl.value, threeThrow);
 
-}
+    if (winnerwinner) {
+        wins++;
+        winorloseEl.textContent = 'Winner Winner, Chicken Dinner!';
+    } else {
+        losses++;
+        winorloseEl.textContent = 'Loser Loser, No good Rhyme!';
+    }
 
-)
+    winwinEl.textContent = `Wins: ${wins}`;
+    loseloseEl.textContent = `Losses: ${losses}`;
+});
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+console.log(yourguess);
+
+resetButtonEl.addEventListener('click', () => {
+    location.reload();
+
+  
+});
--- rps.js
+++ rps.js
@@ -1,11 +1,27 @@
+const sideArray = ['rock', 'paper', 'scissors'];
+
 export function rockpaperscissor(){
 
-    const compHand = Math.round(Math.random() * 2); 
-    if(compHand === 0){
-        return 'rock';
-    } else if(compuHand === 1){
-        return 'paper';
+    const compHand = Math.round(Math.random() * 2);
+    return sideArray[compHand];
+    
+}
+
+export function didUserWin(userThrow, actualThrow) {
+
+    if (userThrow === 'rock' && actualThrow === 'paper'){
+        return userThrow < actualThrow; 
+    } else if ( userThrow === 'paper' && actualThrow === 'rock'){
+        return userThrow > actualThrow;
+    } else if (userThrow === 'rock' && actualThrow === 'scissors'){
+        return userThrow > actualThrow;
+    } if (userThrow === 'scissors' && actualThrow === 'rock'){
+        return userThrow > actualThrow; 
+    } else if ( userThrow === 'paper' && actualThrow === 'scissors'){
+        return userThrow > actualThrow;
+    } else if (userThrow === 'scissors' && actualThrow === 'paper'){
+        return userThrow < actualThrow;
     } else {
-        return 'scissors';
+        return userThrow === actualThrow;
     }
 }
