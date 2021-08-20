c9f23e4c0d1ea492326587c5b6fe1342d41c68cc
EmilyDSarani <emily.sarani@gmail.com>
2021-08-20 11:58:50 -0500

    did a bit of clean-up, but code is still not working

--- app.js
+++ app.js
@@ -2,35 +2,46 @@
 import { rockpaperscissor } from './rps.js';
 import { didUserWin } from './rps.js';
 
+const yourthrow = document.getElementById ('user-choice');
+const digitalthrow = document.getElementById ('computer-choice');
 const goButtonEl = document.getElementById ('pressgo');
 const resetButtonEl = document.getElementById ('reset');
 const winorloseEl = document.getElementById('win-lose');
 const winwinEl = document.getElementById ('wins');
 const loseloseEl = document.getElementById ('lose');
+const drawDrawEl = document.getElementById ('draw');
 
+// eslint-disable-next-line no-unused-vars
+let draws = 0;
+let wins = 0;
+let losses = 0;
 
 
 
 goButtonEl.addEventListener('click', () => {
-    let wins = 0;
-    let losses = 0;
-    let draws = 0;
+   
   
     const computerChoice = rockpaperscissor();
     const selectorEl = document.querySelector('input:checked');
+  
     
-    const winnerwinner = didUserWin (selectorEl, computerChoice);
+    const whoWon = didUserWin (computerChoice, selectorEl);
 
-    if (winnerwinner === 'wins') {
+    if (whoWon === 'wins') {
+        wins++;
         winorloseEl.textContent = 'Winner Winner, Chicken Dinner!';
-    } else if (winnerwinner === 'losses') {
+    } else if (whoWon === 'losses') {
+        losses++;
         winorloseEl.textContent = 'Loser Loser, No good Rhyme!';
+    } else if (whoWon === 'draws'){
+        draws++;
+        winorloseEl.textContent = 'Issa draaaaw';
     }
 
     winwinEl.textContent = `Winner: ${wins}`;
     loseloseEl.textContent = `Loser: ${losses}`;
+    drawDrawEl.textContent = `Draws: ${draws}`;
 
-    console.log(rockpaperscissor);
 });
 
 resetButtonEl.addEventListener('click', () => {
